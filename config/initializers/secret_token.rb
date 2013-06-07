aae8e7d92fa3dc631cde9eda050c8db867c16ed5
ids-tech <msdunnigan@gmail.com>
2013-06-07 10:33:30 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BeTheBeagle::Application.config.secret_token = 'c4369c5f5b890677fab3c4c1fe2e817945e4c38ebe9b524896a000d9f7c6cb984b4ea8720e4f8d4c5918694f6b677e679e0dce128b9d5fa1ae52b76971fe17b5'
