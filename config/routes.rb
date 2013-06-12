534d77f6345fb14981e9ed1aabfc4e2bcd4d3931
ids-tech <msdunnigan@gmail.com>
2013-06-12 11:41:08 +0200

    Added home page and footer

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 BeTheBeagle::Application.routes.draw do
+  root to: 'static_pages#home'
+
+  match '/home',    to: 'static_pages#home'
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/blob',    to: 'no_format_pages#blob'
+
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
   get "no_format_pages/blob"
-
-  root to: 'no_format_pages#blob'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
