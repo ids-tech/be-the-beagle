92aa8cbaa94853c575df2116d0979318f4a04350
ids-tech <msdunnigan@gmail.com>
2013-07-16 18:53:10 +0200

    Finish sign in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,21 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709200105) do
+ActiveRecord::Schema.define(:version => 20130716153449) do
+
+  create_table "employee", :id => false, :force => true do |t|
+    t.string "name"
+    t.string "dept"
+    t.string "jobtitle"
+  end
+
+  create_table "tasks", :id => false, :force => true do |t|
+    t.integer  "id",         :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "name"
+    t.datetime "total_time"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,8 +33,10 @@ ActiveRecord::Schema.define(:version => 20130709200105) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
